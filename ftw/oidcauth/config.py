676ad670cd095d7e19cc00e5092611676d3a5f73
Emmanuel Nyachoke <achachiez@gmail.com>
2021-11-14 12:45:31 +0300

    Catch file not found error

--- ftw/oidcauth/config.py
+++ ftw/oidcauth/config.py
@@ -12,26 +12,29 @@ def configure_oauth(event):
     app.REQUEST["PARENTS"] = [app]
     setRequest(app.REQUEST)
     container = app.unrestrictedTraverse("/")
-    oauth_config_file = os.getenv("OAUTH_CONFIG_FILE", "")
+    oauth_config_file = os.getenv("OAUTH_CONFIG_FILE", "client.json")
     config_file = os.path.join(oauth_config_file)
     if Path(config_file).exists():
-        f = open(config_file)
-        config = json.load(f)
-        for plonesite in container.objectValues("Plone Site"):
-            pas = plonesite.acl_users
-            plugin = pas.get(DEFAULT_ID_OIDC)
-            site_config = config[plonesite.getId()]
-            print(site_config)
-
-            if plugin:
-                for key in site_config:
-                    value = site_config[key]
-                    if key=='properties_mapping':
-                        props_data=plugin.get_valid_json(json.dumps(site_config[key]))
-                        value = props_data
-                    if key=='roles':
-                        roles = site_config[key]
-                        plugin._roles = tuple([role.strip() for role in roles.split(',') if role])
-                    plugin._setPropValue(key, value)
-        transaction.commit()
-        f.close()
+        try:
+            f = open(config_file)
+            config = json.load(f)
+            for plonesite in container.objectValues("Plone Site"):
+                pas = plonesite.acl_users
+                plugin = pas.get(DEFAULT_ID_OIDC)
+                site_config = config[plonesite.getId()]
+                if plugin:
+                    for key in site_config:
+                        value = site_config[key]
+                        if key=='properties_mapping':
+                            props_data=plugin.get_valid_json(json.dumps(site_config[key]))
+                            value = props_data
+                        if key=='roles':
+                            roles = site_config[key]
+                            plugin._roles = tuple([role.strip() for role in roles.split(',') if role])
+                        plugin._setPropValue(key, value)
+            transaction.commit()
+            f.close()
+        except FileNotFoundError:
+            print("Config File does not exist , leaving unconfigured")
+        except:
+            print("Unknown Error leaving unconfigured")
