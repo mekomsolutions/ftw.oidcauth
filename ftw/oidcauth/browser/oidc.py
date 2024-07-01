22e9dd81c1f096c4928d770a7bc1f8f93a455936
wluyima <wyclif@openmrs.org>
2024-07-01 11:49:45 +0300

    When a user logs out they should also be logged out of the authentication provider #12
    Implemented post provider logout callback handler #12

--- ftw/oidcauth/browser/oidc.py
+++ ftw/oidcauth/browser/oidc.py
@@ -1,7 +1,10 @@
+from Products.CMFPlone.browser.login.logout import LogoutView
 from Products.Five import BrowserView
 from ftw.oidcauth.browser.oidc_tools import OIDCClientAuthentication
 from ftw.oidcauth.errors import OIDCBaseError
+from six.moves.urllib.parse import parse_qsl, urlencode
 from zExceptions import NotFound as zNotFound
+from zope.component import getMultiAdapter
 from zope.interface import implements
 from zope.publisher.interfaces import IPublishTraverse
 from zope.publisher.interfaces import NotFound
@@ -23,6 +26,8 @@ class OIDCView(BrowserView):
         if self.method is None:
             if name == 'callback':
                 self.method = name
+            elif name == 'logout':
+                self.method = name
             else:
                 raise NotFound(self, name, request)
         else:
@@ -32,6 +37,8 @@ class OIDCView(BrowserView):
     def __call__(self):
         if self.method == 'callback':
             self.callback()
+        elif self.method == 'logout':
+            self.logout()
         else:
             raise zNotFound()
 
@@ -58,3 +65,45 @@ class OIDCView(BrowserView):
         response.setHeader('Content-Type', 'text/plain')
         response.setStatus(status, lock=1)
         response.setBody(message, lock=1)
+
+    def logout(self):
+        p = OIDCClientAuthentication.get_oidc_plugin()
+        base_url = get_base_url(self.context, self.request)
+        original_redirect = self.request.get('redirect')
+        redirect = base_url
+        if original_redirect:
+            if original_redirect.startswith("http:") or original_redirect.startswith("https:"):
+                redirect = original_redirect
+            else:
+                redirect = base_url + original_redirect
+
+        logout_base_url = p.end_session_endpoint
+        params = {}
+        if "?" in p.end_session_endpoint:
+            url_parts = p.end_session_endpoint.split("?")
+            logout_base_url = url_parts[0]
+            params.update(dict(parse_qsl(url_parts[1])))
+        params["client_id"] = p.client_id
+        params["post_logout_redirect_uri"] = redirect
+        logout_url = "{}?{}".format(logout_base_url, urlencode(params))
+        self.request.response.redirect(logout_url)
+
+
+class OIDCLogoutView(LogoutView):
+    def __call__(self):
+        if OIDCClientAuthentication.get_oidc_plugin().end_session_endpoint:
+            base_url = get_base_url(self.context, self.request)
+            next_ = self.request.get('next')
+            oidc_logout = base_url + '/oidc/logout'
+            if next_ is None or not next_.startswith(oidc_logout):
+                if next_:
+                    oidc_logout = "{}?{}".format(oidc_logout, urlencode({'redirect': next_}))
+                redirect = "{}?{}".format(base_url + '/logout', urlencode({'next': oidc_logout}))
+                self.request.response.redirect(redirect)
+                return
+
+        super(OIDCLogoutView, self).__call__()
+
+
+def get_base_url(context, request):
+    return getMultiAdapter((context, request), name="plone_portal_state").navigation_root_url()
